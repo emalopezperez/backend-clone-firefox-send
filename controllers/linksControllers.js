6a8a356dd87310499a633b7cb37bb78015008be7
emanuel lopez <emanuel-lopez13@hotmail.com>
2023-03-24 15:15:32 -0300

    Add function to save links to the database

--- controllers/linksControllers.js
+++ controllers/linksControllers.js
@@ -1,4 +1,16 @@
+const Links = require("../models/Links")
+const shortid = require("shortid")
+
 exports.nuevoLink = async (req, res, next) => {
-  
-  
-} 
+
+  const { nombre_original, password } = req.body
+
+  const enlaces = new Links()
+
+  enlaces.url = shortid.generate()
+  enlaces.nombre = shortid.generate()
+  enlaces.nombre_original = nombre_original
+  enlaces.password = password
+
+  console.log(enlaces)
+}
