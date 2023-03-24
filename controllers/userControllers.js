f426f615a188266c95098d8c8c396819f051a3d5
emanuel lopez <emanuel-lopez13@hotmail.com>
2023-03-24 09:25:33 -0300

    Added user login and password hashing. Implemented new user authentication flow for improved security

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,12 +1,31 @@
-const UserScheme = require("../models/Usuario")
+const UserScheme = require("../models/Usuario");
+const bcrypt = require('bcrypt');
 
 exports.nuevoUsuario = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  const user =await new UserScheme(req.body)
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Por favor proporcione correo electrónico y contraseña" });
+    }
 
-  user.save()
-  
-  res.json({
-    msg:"usuario creado"
-  })
-}
+    const existingUser = await UserScheme.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ msg: "Usuario ya registrado" });
+    }
+
+    // Hashear password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password.toString(), salt);
+
+    // Crear usuario
+    const newUser = new UserScheme({ email, password: hashedPassword });
+    await newUser.save();
+
+    res.json({ msg: "Usuario creado" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Ocurrió un error al crear un nuevo usuario" });
+  }
+};
